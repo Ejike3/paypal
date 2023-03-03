d04e5113fae89dc6b98a6eb3794f8ff9186a2271
Ejike3 <126031821+Ejike3@users.noreply.github.com>
2023-03-03 17:40:54 +0100

    bug fixed

--- app.java
+++ app.java
@@ -3,3 +3,4 @@ The day Obi spoke to Obidients
 He assured Obidients that he will reclaim his stollen mandate
 He said "your means of arrivig to power must be excellent in other for you to be refered to as His Excelence"
 Peter Gregory Obi is obviously the choice of the people!
+bug fixed
