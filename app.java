081205fcebbba1ea009318f7dd120ff79d4ece75
Ejike3 <126031821+Ejike3@users.noreply.github.com>
2023-03-03 17:34:06 +0100

    Obidient

--- app.java
+++ app.java
@@ -2,4 +2,4 @@ Today is 2nd day of  March, 2023
 The day Obi spoke to Obidients
 He assured Obidients that he will reclaim his stollen mandate
 He said "your means of arrivig to power must be excellent in other for you to be refered to as His Excelence"
-Peter Gregory Obi is obviously the choice of the people
+Peter Gregory Obi is obviously the choice of the people!
